5e11e2a1949086fc370ab97567177ade45676947
Mohammed Hasan <mo70hasan@gmail.com>
2025-09-26 09:50:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+
+    static string[] slots = new string[100]; // Array to hold slot values
+    static string[] v(String s) => string.IsNullOrEmpty(s) ? Array.Empty<string>() : s.Split(('|'); //Split slots into vehicles
+    static bool Empty(int i) => string.IsNullOrEmpty(slots[i]); // Check if a slot is empty
+    static bool IsCar(int i) => !Empty(i) && slots[i].StartsWith("CAR#"); // Check if slot contain a car
+    static bool IsMc(int i) => !Empty(i) && slots[i].StartsWith("MC#"); // Check if slot contain a Mc
+
+
+
+
+}
